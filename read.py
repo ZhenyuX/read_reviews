8f588ca1f5979f52a63e972015193f2cbae1203c
Zhenyu Xu <zhenyuxu112@gmail.com>
2018-08-26 23:52:30 -0700

    计算avg

--- read.py
+++ read.py
@@ -1,13 +1,15 @@
 data = []
 count = 0
-count = int(count)
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
-print(len(data))
-print(data[0])
-print('---------------------')
-print(data[1])
+print('读取完成, 共', len(data), '笔资料')
+
+s = 0
+for i in data:
+	s = s + len(i)
+	avg = s / len(data)
+print('avg=', avg)
